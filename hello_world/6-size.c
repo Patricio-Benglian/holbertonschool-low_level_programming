970801ba7454ed157ba2474f30d42fc6ddc2d3d5
Patricio-Benglian <6305@holbertonstudents.com>
2023-02-15 12:45:30 -0800

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 		printf("Size of an int: 4 byte(s)\n");
 		printf("Size of a long int: 4 byte(s)\n");
 		printf("Size of a long long int: 8 byte(s)\n");
-		printf("Size of a float: 4 byte(s)\n");
+		printf("Size of a float: 4 byte(s)");
 	}
 	else
 	{
@@ -20,7 +20,7 @@ int main(void)
 		printf("Size of an int: 4 byte(s)\n");
 		printf("Size of a long int: 8 byte(s)\n");
 		printf("Size of a long long int: 8 byte(s)\n");
-		printf("Size of a float: 4 byte(s)\n");
+		printf("Size of a float: 4 byte(s)");
 	}
 	return (0);
 }
