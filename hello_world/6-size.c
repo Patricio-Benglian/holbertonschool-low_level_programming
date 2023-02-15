cf811df5fa89a85824fa74c44a182b2d0b0585b1
Patricio-Benglian <6305@holbertonstudents.com>
2023-02-15 12:43:47 -0800

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,19 +8,19 @@ int main(void)
 {
 	if ("size32" == __FILE__)
 	{
-		printf("Size of a char: 1 bytes(s)");
-		printf("Size of an int: 4 byte(s)");
-		printf("Size of a long int: 4 byte(s)");
-		printf("Size of a long long int: 8 byte(s)");
-		printf("Size of a float: 4 byte(s)");
+		printf("Size of a char: 1 bytes(s)\n");
+		printf("Size of an int: 4 byte(s)\n");
+		printf("Size of a long int: 4 byte(s)\n");
+		printf("Size of a long long int: 8 byte(s)\n");
+		printf("Size of a float: 4 byte(s)\n");
 	}
 	else
 	{
-		printf("Size of a char: 1 bytes(s)");
-		printf("Size of an int: 4 byte(s)");
-		printf("Size of a long int: 8 byte(s)");
-		printf("Size of a long long int: 8 byte(s)");
-		printf("Size of a float: 4 byte(s)");
+		printf("Size of a char: 1 bytes(s)\n");
+		printf("Size of an int: 4 byte(s)\n");
+		printf("Size of a long int: 8 byte(s)\n");
+		printf("Size of a long long int: 8 byte(s)\n");
+		printf("Size of a float: 4 byte(s)\n");
 	}
 	return (0);
 }
