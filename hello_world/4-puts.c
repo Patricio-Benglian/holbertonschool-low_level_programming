ec95cd9e49780637b6088121ab645948a7f49437
Patricio-Benglian <6305@holbertonstudents.com>
2023-02-15 10:13:42 -0800

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - writes the stuff in the quotes
+ * puts - outputs the text in the quotes
+ * return - halts execution of the function
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
