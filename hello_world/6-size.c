c33fa26f322400a139cb77384026560bae1a09bf
Patricio-Benglian <6305@holbertonstudents.com>
2023-02-15 12:40:18 -0800

    size

---
+++ hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	if (__FILE__ == size32)
+	{
+		printf("Size of a char: 1 bytes(s)");
+		printf("Size of an int: 4 byte(s)");
+		printf("Size of a long int: 4 byte(s)");
+		printf("Size of a long long int: 8 byte(s)");
+		printf("Size of a float: 4 byte(s)");
+	}
+	else
+	{
+		printf("Size of a char: 1 bytes(s)");
+                printf("Size of an int: 4 byte(s)");
+                printf("Size of a long int: 8 byte(s)");
+                printf("Size of a long long int: 8 byte(s)");
+                printf("Size of a float: 4 byte(s)");
+	}
+	return (0);
+}
