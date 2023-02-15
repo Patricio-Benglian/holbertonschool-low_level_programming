0ab3e7c62bb3eff6ca9f5eb08ee56bbd8b422a68
Patricio-Benglian <6305@holbertonstudents.com>
2023-02-15 12:41:42 -0800

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	if (__FILE__ == size32)
+	if (size32 == __FILE__)
 	{
 		printf("Size of a char: 1 bytes(s)");
 		printf("Size of an int: 4 byte(s)");
@@ -17,10 +17,10 @@ int main(void)
 	else
 	{
 		printf("Size of a char: 1 bytes(s)");
-                printf("Size of an int: 4 byte(s)");
-                printf("Size of a long int: 8 byte(s)");
-                printf("Size of a long long int: 8 byte(s)");
-                printf("Size of a float: 4 byte(s)");
+		printf("Size of an int: 4 byte(s)");
+		printf("Size of a long int: 8 byte(s)");
+		printf("Size of a long long int: 8 byte(s)");
+		printf("Size of a float: 4 byte(s)");
 	}
 	return (0);
 }
