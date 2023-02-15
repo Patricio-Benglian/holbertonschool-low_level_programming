a517a3acf013f03eac00e6cbac46d51cfc4491c3
Patricio-Benglian <6305@holbertonstudents.com>
2023-02-15 12:52:38 -0800

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Size of a char: 1 bytes(s)\n");
+		printf("Size of a char: 1 byte(s)\n");
 		printf("Size of an int: 4 byte(s)\n");
 		printf("Size of a long int: 8 byte(s)\n");
 		printf("Size of a long long int: 8 byte(s)\n");
