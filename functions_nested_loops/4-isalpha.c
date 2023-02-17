4531f4c8d68a5c866bc5da46e3a607e477cf7e90
Patricio-Benglian <6305@holbertonstudents.com>
2023-02-17 08:43:34 -0800

    isalpha

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _islower - returns 1 if its lowercase
+ * _isalpha - returns 1 if its lowercase
  * @c: integer
  * Return: Always 0 (Success)
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90)
 	{
