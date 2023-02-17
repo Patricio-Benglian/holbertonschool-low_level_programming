0db7ae353ba527d7fbdb6fa632a9af8a065f6842
Patricio-Benglian <6305@holbertonstudents.com>
2023-02-17 06:16:55 -0800

    alphax10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 	char alpha;
 	int loop;
 	
-	for (loop = 0; loop <= 10; loop++)
+	for (loop = 1; loop <= 10; loop++)
 	{
 		for (alpha = 'a'; alpha <= 'z' ; alpha++)
 		{
