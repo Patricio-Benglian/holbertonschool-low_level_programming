91c9acafe335b0fe0f6e7f0b3df19c88a615f2ce
Patricio-Benglian <6305@holbertonstudents.com>
2023-02-17 06:05:28 -0800

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - entry
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z' ; alpha++)
+	{
+		_putchar(alpha);
+	}
+}
