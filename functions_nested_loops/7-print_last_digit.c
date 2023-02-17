6aa3eac9f853d8abfac7e9a80da8c9569bd4d9c9
Patricio-Benglian <6305@holbertonstudents.com>
2023-02-17 09:51:27 -0800

    printlastdigittwice

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -6,7 +6,16 @@
  */
 int print_last_digit(int n)
 {
-	n = n % 10;
-	_putchar(n + '0');
-	return (n);
+	if (n >= 0)
+	{
+		n = n % 10;
+		_putchar(n + '0');
+		return (n);
+	}
+	else
+	{
+		n = n % (-10);
+		_putchar(n + '0');
+		return (n);
+	}
 }
