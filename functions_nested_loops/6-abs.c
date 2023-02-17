8cce826e76e83b3d613c91e02ca59a67aed49e72
Patricio-Benglian <6305@holbertonstudents.com>
2023-02-17 09:12:05 -0800

    absvalue

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an int
+ * @r: integer to computer
+ * Return: Always 0 (Success)
+ */
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		r = r * -1;
+	}
+	else
+	{
+		r = r;
+	}
+}
