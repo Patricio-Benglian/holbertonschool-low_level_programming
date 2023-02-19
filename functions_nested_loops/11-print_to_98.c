23b9bc2cf9a590b9a3844d4640942af44ebe4a9c
Patricio-Benglian <6305@holbertonstudents.com>
2023-02-19 04:11:36 -0800

    to98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,16 +9,22 @@ void print_to_98(int n)
 	_putchar(n);
 	if (n < 98)
 	{
-		for (; n <= 98; n++)
+		for (; n < 98; n++)
 		{
 			_putchar(n);
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
 	else if (n > 98)
 	{
-		for (; n >= 98; n--)
+		for (; n > 98; n--)
 		{
 			_putchar(n);
+			_putchar(',');
+			_putchar(' ');
 		}
+	}
+	_putchar(98);
 
 }
