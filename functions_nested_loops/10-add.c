ae5c3673475094567dd9b4df210909aeb6aa9a8f
Patricio-Benglian <6305@holbertonstudents.com>
2023-02-19 03:55:24 -0800

    add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds both integers
+ * @add1: first integer to add
+ * @add2: second integer to add
+ * Return: Always sum of both integers (Success)
+ */
+int add(int add1, int add2)
+{
+	int sum;
+
+	_putchar(sum = add1 + add2);
+	return (sum);
+}
