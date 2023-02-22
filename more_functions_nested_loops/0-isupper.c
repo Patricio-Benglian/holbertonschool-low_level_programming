e54cd5294456db27bc9d166636df155da0db8491
Patricio-Benglian <6305@holbertonstudents.com>
2023-02-22 09:34:55 -0800

    isupper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
+ * _isupper - returns 1 if int c is an uppercase letter
+ * @c: variable
  * Return: Always 0 (Success)
  */
 int _isupper(int c)
 {
-	if (c + '0' >= 'A' && c + '0' <= 'Z')
+	if (c + '0' >= 65 && c + '0' <= 90)
 	{
 		return (1);
 	}
