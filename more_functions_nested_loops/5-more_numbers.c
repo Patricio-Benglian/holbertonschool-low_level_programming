441311d39f67b2fa81eeb2514d77dfe092358b2d
Patricio-Benglian <6305@holbertonstudents.com>
2023-02-22 10:29:43 -0800

    print10x

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * more_numbers - prints 0-14 ten times
+ * Return: Always 0 (Success)
+ */
+void more_numbers(void)
+{
+	int i;
+	int r;
+
+	for (r = 0; r <= 9; r++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+
+}
