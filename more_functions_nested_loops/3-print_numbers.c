851985b09c237f3423c22b2df08852c6919bc371
Patricio-Benglian <6305@holbertonstudents.com>
2023-02-22 09:59:03 -0800

    printnumbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0, i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
