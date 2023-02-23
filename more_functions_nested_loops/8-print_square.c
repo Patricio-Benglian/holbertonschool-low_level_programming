334035f7682639b29e4d1b20a1b60f29091eb39b
Patricio-Benglian <6305@holbertonstudents.com>
2023-02-23 04:09:10 -0800

    make a square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_square - prints a square of size size
+ * @size: determines the length of the sides of the square
+ */
+void print_square(int size)
+{
+	int h, w;
+
+	if (size == 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	for (h = 0; h < size; h++)
+	{
+		for (w = 0; w < size; w++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	}
+}
