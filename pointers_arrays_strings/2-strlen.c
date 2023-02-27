86f42ce5a184e3b8282fa045b3aa85b05349512d
Patricio-Benglian <6305@holbertonstudents.com>
2023-02-27 09:27:22 -0800

    Strlen

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -4,13 +4,13 @@
 /**
  * _strlen - returns the length of a string
  * @s: string
- * return - always returns 0
+ * return: returns count
  */
 int _strlen(char *s)
 {
 	int count = 0;
 
-	while (*s != '\0') 
+	while (*s != '\0')
 	{
 		s++;
 		count++;
