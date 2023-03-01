5bad0450c6686dbc3b8718acf5741894ef38ed13
Patricio-Benglian <6305@holbertonstudents.com>
2023-03-01 08:23:57 -0800

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,12 @@ int _strcmp(char *s1, char *s2)
 {
 	while (*s1 == *s2)
 	{
-			s1++;
-			s2++;
-			if (*s1 == '\0')
-			{
-				return (0);
-			}
+		s1++;
+		s2++;
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
 	}
-	return ((*s1 + '0')- (*s2 + '0'));
+	return ((*s1 + '0') - (*s2 + '0'));
 }
