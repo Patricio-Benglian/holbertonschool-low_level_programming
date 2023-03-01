4bb448b4f00a97a7639f397d40d832c49c1d30c0
Patricio-Benglian <6305@holbertonstudents.com>
2023-03-01 07:00:26 -0800

    Strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * *_strncopy - copies string to n
+ * _strncpy - copies string to n
  * @dest: destination
  * @src: source
  * @n: limit of concatenation for src
  * Return: product
  */
-char *_strncopy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
