f63df2ac1d1f0355f862bd42a9162539a762c8b2
Patricio-Benglian <6305@holbertonstudents.com>
2023-03-02 13:57:22 -0800

    AtoI

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,34 +7,43 @@
  */
 int _atoi(char *s)
 {
-	int neg = 0, newnum2 = 0, tog = 0;
+	int neg = 0, newnum2 = 0, tog = 0, i;
 	unsigned int newnum = 0;
 
-	while (*s > 57 || *s < 48)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-	if (*s == '-')
-	{
-		neg++;
+		if (s[i] >= 48 && s[i] <= 57)
+		{
+			tog = 1;
+		}
 	}
-	s++;
-	}
-	while (*s - '0' >= 0 && *s - '0' <= 9)
+	if (tog == 1)
 	{
-		newnum = newnum * 10 + (*s - '0');
+		while (*s > 57 || *s < 48)
+		{
+			if (*s == '-')
+			{
+				neg++;
+			}
 		s++;
-		tog = 1;
-	}
-	if (tog == 0)
-	{
-		return (tog);
-	}
-	if (neg % 2 == 1)
-	{
-		newnum2 = newnum * -1;
+		}
+		while (*s - '0' >= 0 && *s - '0' <= 9)
+		{
+			newnum = newnum * 10 + (*s - '0');
+			s++;
+		}
+		if (neg % 2 == 1)
+		{
+			newnum2 = newnum * -1;
+		}
+		else
+		{
+			newnum2 = newnum;
+		}
+		return (newnum2);
 	}
 	else
-	{
-		newnum2 = newnum;
-	}
-	return (newnum2);
+		{
+			return (tog);
+		}
 }
