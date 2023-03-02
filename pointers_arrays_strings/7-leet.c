5a4bd8489d80e9ec25334a8393699328b0c3dac0
Patricio-Benglian <6305@holbertonstudents.com>
2023-03-02 08:07:08 -0800

    1337

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 #include "main.h"
+#include "2-strlen.c"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * leet - turns a string into 1337sp34k
+ * @l: unleeted string
+ * Return: converted string
  */
 char *leet(char *l)
 {
+	int i, n;
+	char aeotl[] = {65, 69, 79, 84, 76};
+	char ctcsu[] = {52, 51, 48, 55, 49};
 
+	for (i = 0; *l; i++)
+	{
+		for (n = 0; n < 5; n++)
+		{
+			if (*l == aeotl[n] || *l == aeotl[n] + 32)
+			{
+				*l = ctcsu[n];
+			}
+		}
+		n = 0;
+		l++;
+	}
+	return (l);
 }
