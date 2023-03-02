59a487b7a8d0d32a4976d9c3b7ec15e3de36f3d3
Patricio-Benglian <6305@holbertonstudents.com>
2023-03-02 06:06:26 -0800

    Capitalize

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
 #include "main.h"
+#include "2-strlen.c"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * cap_string - capitalizes the words of a string
+ * @cap: string
+ * Return: Capitalized string
  */
 char *cap_string(char *cap)
 {
+	int i, n, count, togcaps = 1;
+	char sp[] = {32, 9, 10, 44, 59, 34, 40, 41, 123, 125, 46, 33, 63};
 
+	count = _strlen(cap);
+	for (i = 0; i < count; i++)
+	{
+		if (cap[i] >= 'a' && cap[i] <= 'z' && togcaps == 1)
+		{
+			cap[i] = cap[i] - 32;
+		}
+		togcaps = 0;
+		for (n = 0; sp[n]; n++)
+		{
+			if (cap[i] == sp[n])
+			{
+				togcaps = 1;
+			}
+		}
+	}
+	return (cap);
 }
