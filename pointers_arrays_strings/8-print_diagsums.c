e71e0ce76abb9df36707c39a956b80f2d0e556a7
Patricio-Benglian <6305@holbertonstudents.com>
2023-03-03 09:19:52 -0800

    DiagSum

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -7,5 +7,20 @@
  */
 void print_diagsums(int *a, int size)
 {
+	int i, n, sum = 0, sum2 = 0;
 
+	for (i = 0, n = 0; i < size;)
+	{
+		sum = sum + a[i * size + n];
+		i++;
+		n++;
+	}
+	i = 0;
+	for (i = 0, n = size - 1; i < size;)
+	{
+		sum2 = sum2 + a[i * size + n];
+		i++;
+		n--;
+	}
+	printf("%d, %d\n", sum, sum2);
 }
