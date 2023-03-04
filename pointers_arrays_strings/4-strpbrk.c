9565bc080b98e453485f6c86a0817372364d1cf1
Patricio-Benglian <6305@holbertonstudents.com>
2023-03-04 08:21:02 -0800

    StrpBrk

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _strpbrk - searches a string for any of a set of bytes
+ * _strpbrk - searches a string for any set of bytes
  * @s: string
- * @accept: set of bytes
- * Return: returns pointer to first match or null
+ * @accept: prefix characters
+ * Return: number of bytes matching prefix
  */
 char *_strpbrk(char *s, char *accept)
 {
+	unsigned int i, n;
 
+	for (i = 0, n = 0; *accept;)
+	{
+		if (accept[n] == s[i])
+		{
+			return (s + i - 1);
+		}
+		else if (accept[n + 1] == '\0')
+		{
+			return (s + i + 1);
+		}
+		else
+		{
+			n++;
+			i++;
+		}
+	}
+	return (s + i - 1);
 }
