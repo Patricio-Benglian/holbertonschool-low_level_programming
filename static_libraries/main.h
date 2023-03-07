f9b69b27ee44f58826e5c76adc01e061e1109592
Patricio-Benglian <6305@holbertonstudents.com>
2023-03-07 07:45:23 -0800

    l

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#ifndef <main.h>
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -20,4 +19,3 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
