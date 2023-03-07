a4601820d6fe51f4e615fc7a6dec7f0f03cf78f0
Patricio-Benglian <6305@holbertonstudents.com>
2023-03-07 09:01:39 -0800

    End IF

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void FileNameIs(char **argv)
+{
+	printf("%s\n", argv[0]);
+}
---
+++ argc_argv/main.h
@@ -0,0 +1,4 @@
+#include <unistd.h>
+int _putchar(char c);
+void FileNameIs(char **argv);
+int print_arg_count(int argc, char **argv);
