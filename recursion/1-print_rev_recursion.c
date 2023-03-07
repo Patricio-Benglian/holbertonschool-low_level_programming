b33179b25eca24e6aedcf4b8b093081f28806f0e
Patricio-Benglian <6305@holbertonstudents.com>
2023-03-07 03:46:44 -0800

    Print Reverse Recursion

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -8,11 +8,12 @@ void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
+		if (s[1] == '0')
+		{
+			_putchar('\n');
+		}
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
-	else
-	{
-		_putchar('\n');
-	}
+
 }
