1290a25cf1d1166bc1765a1bd49b2f39b62fdd8f
Patricio-Benglian <6305@holbertonstudents.com>
2023-03-09 04:03:03 -0800

    Sqrt

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -10,15 +10,34 @@ int root(int n, int i)
 {	
 	if (n == i * i)
 	{
-		return i;
+		return (i);
+	}
+	if (i * i < n)
+	{
+		return root(n, i + 1);
 	}
 	else
 	{
-		return root(n, i + 1);
+		return (-1);
 	}
 }
 
 int _sqrt_recursion(int n)
 {
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else if (n == 1)
+	{
+		return (1);
+	}
+	else
+	{
 	return root(n, 2);
+	}
 }
