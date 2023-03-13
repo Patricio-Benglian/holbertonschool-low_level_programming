772fc5793c60c5e04b8051bf4eb29c7c07bb02f2
Patricio-Benglian <6305@holbertonstudents.com>
2023-03-13 14:07:32 -0700

    Strdup

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -2,11 +2,31 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * char *_strdup(char *str) - Entry point
+ * _strdup - returns pointer to a copy of string
  * @str: string
  * Return: Always 0 (Success)
  */
 char *_strdup(char *str)
 {
+	unsigned int i, length;
+	char *arr;
 
+	if (!str)
+	{
+		return (NULL);
+	}
+
+	for (length = 0; str[length]; length++)
+	{
+		;
+	}
+
+	arr = malloc(sizeof(str) * length + 1);
+
+	for (i = 0; i < length; i++)
+	{
+		arr[i] = str[i];
+	}
+
+	return (arr);
 }
