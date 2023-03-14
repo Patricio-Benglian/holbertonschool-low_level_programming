bbe7bf3ebaf0b6af868965a71fac5d3353a38753
Patricio-Benglian <6305@holbertonstudents.com>
2023-03-14 12:39:11 -0700

    ConCat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,16 +12,16 @@ char *str_concat(char *s1, char *s2)
 	int l, l2, i, i2; /* var to calc s1 and s2 length, incrementors*/
 	char *cat; /* concatenated string */
 
-	if (!s1 && !s2) /* if both empty return NULL */
-		return (NULL);
-	if (!s1)
+	if (!s1) /* replaces NULL with empty string for concat purposes */
 		s1 = "";
-	if (!s2)
+	if (!s2) /* same as above */
 		s2 = "";
+
 	for (l = 0; s1[l]; l++) /* calc len of s1 */
 		;
 	for (l2 = 0; s2[l2]; l2++) /* calc len of s2 */
 		;
+
 	cat = malloc(sizeof(char) * (l + l2) + 1);
 	if (!cat)
 		return (NULL);
