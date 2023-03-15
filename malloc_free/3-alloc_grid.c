8dd4dc726ada205a46660f944456ad84fbd1ed03
Patricio-Benglian <6305@holbertonstudents.com>
2023-03-15 16:55:13 -0700

    a

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -13,19 +13,33 @@ int **alloc_grid(int width, int height)
 
 	if (width < 1 || height < 1) /* if dimensions are 0 return NULL */
 		return (NULL);
-	array = malloc(sizeof(int) * height); /* allo mem for dimensions */
-	for (i = 0; i < width; i++)
+
+	/* allocate memory for first dimension of array */
+	array = malloc(sizeof(int *) * height);
+
+	if (!array)
+		return (NULL);
+
+	/* allocate memory for second dimension of array */
+	for (i = 0; i < height; i++)
 	{
 		array[i] = malloc(sizeof(int) * width);
+		/* another mem check */
 		if (!array[i])
+		{
+			for (i2 = 0; i2 < i ; i2++)
+			{
+				free(array[i2]);
+			}
+			free(array);
 			return (NULL);
+		}
 	}
 
-	if (!array) /* mem check */
-		return (NULL);
 	/* set all values in array to 0 */
 	for (i = 0; i < height; i++)
 		for (i2 = 0; i2 < width; i2++)
 			array[i][i2] = 0;
+
 	return (array);
 }
