ae71580412d81086b051c3a255d825d2ef19ac8f
Patricio-Benglian <6305@holbertonstudents.com>
2023-03-15 09:38:19 -0700

    Allocate Array

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -9,19 +9,18 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int **array, i;
+	int **array, i, i2; /* array, iterators */
 
 	if (width < 1 || height < 1) /* if dimensions are 0 return NULL */
 		return (NULL);
-	*array = malloc(sizeof(int) * height); /* allo mem for first dimension */
+	array = malloc(sizeof(int) * height); /* allo mem for dimensions */
+	for (i = 0; i < width; i++)
+		array[i] = malloc(sizeof(int) * width);
 	if (!array) /* mem check */
 		return (NULL);
-	for (i = 0; i < height; i++) /* allo mem for second dimension */
-	{
-		array[i] = malloc(sizeof(int) * width);
-		if (!array[i]) /* mem check */
-			return (NULL);
-	}
 	/* set all values in array to 0 */
+	for (i = 0; i < height; i++)
+		for (i2 = 0; i2 < width; i2++)
+			array[i][i2] = 0;
 	return (array);
 }
---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * void free_grid(int **grid, int height) - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void free_grid(int **grid, int height)
+{
+
+}
