00a2e758d0e892f5a56054cbff972b152e24c105
Patricio-Benglian <6305@holbertonstudents.com>
2023-03-16 05:15:48 -0700

    Malloc Checked Exit Code

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -2,15 +2,20 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * malloc_checked(unsigned int b) - allocates memory
+ * malloc_checked - allocates memory
  * @b: amount of memory allocated
  * Return: Always 0 (Success)
  */
 void *malloc_checked(unsigned int b)
 {
 	int *pt2mem; /* pointer to memory */
+
 	/* allocates amount of memory equal to b */
 	pt2mem = malloc(b);
+
+	/* memory check */
+	if (!pt2mem)
+		return(98);
 
 	return (pt2mem);
 }
