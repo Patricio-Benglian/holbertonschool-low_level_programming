31380412855cf1fd44b151041d9598caaf6a0ced
Patricio-Benglian <6305@holbertonstudents.com>
2023-03-16 04:49:00 -0700

    free grid

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -2,11 +2,19 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * void free_grid(int **grid, int height) - Entry point
- *
+ * free_grid - frees memory allocated for a grid
+ * @grid: the grid
+ * @height: height of the grid
  * Return: Always 0 (Success)
  */
 void free_grid(int **grid, int height)
 {
+	int i; /* loop iterator */
 
+	/* loops through each row of the grid and deallocates mem */
+	for (i = 0; i < height; i++)
+	{
+		free(a[i]);
+	}
+	free(a);
 }
