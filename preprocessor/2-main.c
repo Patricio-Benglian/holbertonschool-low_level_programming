20cda4d23efb0f81ba6e68aeca25d921c30d32b1
Patricio-Benglian <6305@holbertonstudents.com>
2023-03-17 09:36:11 -0700

    Print Filename

---
+++ preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_name - prints file name
+ *
+ * Return: file name
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
