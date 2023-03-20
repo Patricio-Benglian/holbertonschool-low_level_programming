bb6c2daef0b88485233f3aaf8882848e3006507e
Patricio-Benglian <6305@holbertonstudents.com>
2023-03-20 09:10:08 -0700

    init dog

---
+++ structures_typedef/2-�print_dog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * void print_dog(struct dog *d) - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_dog(struct dog *d)
+{
+			
+}
--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -59,3 +59,4 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 int main(void);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
