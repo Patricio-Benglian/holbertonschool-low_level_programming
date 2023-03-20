3257aa81ce84b30db75934328f0f0384182c38e6
Patricio-Benglian <6305@holbertonstudents.com>
2023-03-20 09:19:20 -0700

    rt

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -13,9 +13,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	struct dog;
 
-	if (!d)
-		break;
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d)
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 }
