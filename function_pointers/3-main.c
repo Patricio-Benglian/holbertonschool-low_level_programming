a4558ed181f456fe5b130703b8c26f9bd2107170
Patricio-Benglian <6305@holbertonstudents.com>
2023-03-23 04:25:04 -0700

    uploading for posterity

---
+++ function_pointers/3-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - TBD
+ *
+ * Return: Always 0 (Success)
+ */
+int main(argc, **argv)
+{
+	if (argc != 4)
+	{	printf("Error\n");
+		exit(98);
+	}
+
+}
