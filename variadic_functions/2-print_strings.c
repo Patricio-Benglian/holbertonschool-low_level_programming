65b2f7e685efd94ef3d9a80398cf75a27effe378
Patricio-Benglian <6305@holbertonstudents.com>
2023-03-24 10:28:31 -0700

    PRINT STRINGS

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -10,6 +10,7 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i; /* iterator */
+	char* s; /* contains value of string */
 	va_list args; /* makes a list of all the arguments */
 
 	/* enables access to variadic func args */
@@ -17,17 +18,24 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++) /* iterate n times (once for each arg) */
 	{
+		s = va_arg(args, char*); /* sets value of s to string */
 		/* print string then separator unless its the last number */
 		if (i != n - 1 && separator != NULL)
 		{
-			printf("%s%s", va_arg(args, char*), separator);
+			if (s == NULL) /* if string is null */
+				printf("(nil)%s", separator);
+			else
+				printf("%s%s", s, separator);
 		}
 		/* print only string if its the last one */
 		else
 		{
-			printf("%s", va_arg(args, char*));
+			if (s == NULL) /* if string is null */
+				printf("(nil)");
+			else
+				printf("%s", s);
 		}
-		/* need to check for NULL strings but it breaks heavily when i do it */
 	}
 	printf("\n");
+	va_end(args);
 }
--- variadic_functions/README.md
+++ variadic_functions/README.md
@@ -1,1 +1,4 @@
-placeholder
+EVERY TIME va_arg is called, it iterates to the next argument. therefore, comparisons such as 
+if (va_arg != NULL)
+	printf("%s", va_arg(list, type)
+breaks the system because va_arg will go to the next argument in the IF and then again in the printf, therefore jumping two spaces instead of one.
