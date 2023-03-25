585f9552fb16bd89368899afa108472fd73c7750
Patricio-Benglian <6305@holbertonstudents.com>
2023-03-25 10:21:12 -0700

    Pushing for posterity

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_all - prints anything
+ * @format: list of characters corresponding to vartype of respective arg list
+ */
+void print_all(const char * const format, ...)
+{
+
+}
+
+/**
+ * aux_char - auxiliary function of main to print characters
+ * @list: a copy of va_list from main
+ * @arg: the character in question
+ */
+void aux_char(list, arg)
+{
+
+}
+/**
+ * aux_int - auxiliary function of main to print integers
+ * @list: a copy of va_list from main
+ * @arg: the integer in question
+ */
+void aux_int(list, arg)
+{
+
+}
+/**
+ * aux_float - auxiliary function of main to print floats
+ * @list: a copy of va_list from main
+ * @arg: the float in question
+ */
+void aux_float(list, arg)
+{
+
+}
+
+/**
+ * aux_string - auxiliary function of main to print strings
+ * @list: a copy of va_list from main
+ * @arg: the string in question to print
+ */
+void aux_string(list, *arg)
+{
+
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,7 +1,17 @@
 #ifndef VARIADIC_H
 #define VARIADIC_H
 
+
+/* struct for print_all function */
+typedef struct printer
+{
+	char *type;
+	int (*p)(list, aux);
+} printer_t;
+
+/* prototypes */
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
