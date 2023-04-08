0ef81ca90d50c0c6af8a50cfafdff9fdb68d6552
Patricio-Benglian <6305@holbertonstudents.com>
2023-04-08 16:01:06 -0700

    Task 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	/* iterate to index position */
 	for (iterator = 1; iterator < idx; iterator++)
-		if (temp->next)
+		if (temp)
 			temp = temp->next;
 	/* check if we went outside list */
 	if (!temp)
