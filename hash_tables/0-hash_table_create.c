87f7f8da535160785d89136e6ef2d9e2903e1275
Patricio-Benglian <6305@holbertonstudents.com>
2023-04-12 10:20:35 -0700

    Task 1

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -8,7 +8,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *arr; /* each section of arr is = hash_table_t */
 
-	arr = malloc(sizeof((arr) * size) + 16); /* allocates memory for arr ptr */
+	arr = malloc(sizeof(arr) * size + 16); /* allo mem for arr ptr */
+	/* idk why it was allocated 16 bytes less than necessary */
 	if (!arr)
 		return (NULL);
 	return (arr);
