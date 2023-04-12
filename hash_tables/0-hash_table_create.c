9827ef85f1771c96d7ceff00d23bf3a1b501c4df
Patricio-Benglian <6305@holbertonstudents.com>
2023-04-12 10:14:22 -0700

    Task 1

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -6,8 +6,10 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *arr;
+	hash_table_t *arr; /* each section of arr is = hash_table_t */
 
-	arr = malloc(sizeof(hash_node_t) * size);
+	arr = malloc(sizeof(arr) * size); /* allocates memory for arr ptr */
+	if (!arr)
+		return (NULL);
 	return (arr);
 }
