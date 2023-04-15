feca7971a5a9bb492b7edbcc96c49de724038857
Patricio-Benglian <6305@holbertonstudents.com>
2023-04-15 10:48:44 -0700

    Fixed permissions

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,7 +14,8 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	/* 600 is permissions. 0 before is something else i forget */
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	write(fd, text_content, strlen(text_content));
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (text_content)
+		write(fd, text_content, strlen(text_content));
 	return (1);
 }
