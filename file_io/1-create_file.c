c9a29ef0c36fbe9812c0c2a4caa43c4764c55d52
Patricio-Benglian <6305@holbertonstudents.com>
2023-04-15 10:46:16 -0700

    Task 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: file name
+ * @text_content: content of file
+ * Return: Always 1 (Success)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd; /* file descriptor */
+
+	if (!filename)
+		return (-1);
+	/* 600 is permissions. 0 before is something else i forget */
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	write(fd, text_content, strlen(text_content));
+	return (1);
+}
