90e225cc8681e7c6af764d4666c00d165f6216cf
Patricio-Benglian <6305@holbertonstudents.com>
2023-04-16 08:39:09 -0700

    Task 4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -9,9 +9,20 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	size_t index; /* index of element we return */
+	hash_node_t *temp;
 
+	if (!ht || !key)
+		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[index])
-		return (ht->array[index]->value);
+	{
+		temp = ht->array[index];
+		while (temp != NULL)
+		{
+			if (strcmp(temp->key, key) == 0)
+				return (temp->value);
+			temp = temp->next;
+		}
+	}
 	return (NULL);
 }
