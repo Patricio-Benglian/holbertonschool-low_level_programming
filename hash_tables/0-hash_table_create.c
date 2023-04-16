f9add7045a5f2371ef15997080b250d21a6830b8
Patricio-Benglian <6305@holbertonstudents.com>
2023-04-16 07:45:30 -0700

    Task 1 update

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -7,7 +7,6 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *arr;
-	size_t i;
 
 	/* size of hash table + amount of pointers to nodes needed */
 	arr = malloc(sizeof(hash_table_t));
