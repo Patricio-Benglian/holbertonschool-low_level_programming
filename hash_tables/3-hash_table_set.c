945b1da5d6844998223bed88691548b8ed6abea8
Patricio-Benglian <6305@holbertonstudents.com>
2023-04-16 10:06:28 -0700

    Fixed memory issues

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -38,6 +38,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(ht->array[pos]->key, key) == 0)
 		{
 			temp = ht->array[pos];
+			free(ht->array[pos]->value);
+			free(ht->array[pos]->key);
 			free(ht->array[pos]);
 			ht->array[pos] = pNode;
 			pNode->next = temp->next;
